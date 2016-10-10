7638f42c1de7516c09c6672f8abeadb1e4ec9ecc
nobusugi246 <nobusugi246@gmail.com>
2016-10-10 19:37:31 +0900

    add dummy integration test.

---
+++ modeling/src/integration-test/groovy/gki/model/ModelingSpec.groovy
@@ -0,0 +1,20 @@
+package gki.model
+
+
+import grails.test.mixin.integration.Integration
+import grails.transaction.*
+import spock.lang.*
+
+@Integration
+@Rollback
+class ModelingSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
