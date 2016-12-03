e77d8dc4445109c78dc46f196d0c71eaee08e87b
nobusugi246 <nobusugi246@gmail.com>
2016-12-03 18:10:06 +0900

    downgrade Grails to Ver.3.1.14.

--- chat/grails-app/init/BootStrap.groovy
+++ chat/grails-app/init/BootStrap.groovy
@@ -7,8 +7,11 @@ import groovy.util.logging.Slf4j
 @Slf4j
 class BootStrap {
   def init = { servletContext ->
+    log.info 'started...'
+
     if ( !ChatRoom.get(1) ) {
       new ChatRoom(name: "Entrance").save()
+      log.info "new ChatRoom: Entrance"
     }
 
     try {
@@ -18,6 +21,7 @@ class BootStrap {
       conf.rooms.each { room ->
         if( !ChatRoom.findByName(room) ){
           new ChatRoom(name: room).save()
+          log.info "new ChatRoom: ${room}"
         }
       }
 
@@ -25,6 +29,7 @@ class BootStrap {
         if( !FeedCrawler.findByUrl(feed[1]) ){
           new FeedCrawler(name: feed[0], url: feed[1], chatroom: feed[2],
                           interval: feed[3], enabled: feed[4]).save()
+          log.info "new FeedCrawler: ${feed[0]}"
         }
       }
 
@@ -32,6 +37,7 @@ class BootStrap {
         if( !WebHook.findByHookFrom(hook[1]) ){
           new WebHook(hookName: hook[0], hookFrom: hook[1],
                       chatroom: hook[2], enabled: hook[3]).save()
+          log.info "new WebHook: ${hook[0]}"
         }
       }
 
@@ -40,8 +46,10 @@ class BootStrap {
           new Jenkins(name: jenkins[0], url: jenkins[1],
                       username: jenkins[2], password: jenkins[3],
                       created: jenkins[4], enabled: jenkins[5]).save()
+          log.info "new Jenkins: ${jenkins[0]}"
         }
       }
+    log.info 'done'
     } catch (e) {
       log.info e.message
     }
