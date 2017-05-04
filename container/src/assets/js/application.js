b415fafa9865dfa0c191a64a461a4bf47f8ba3db
nobusugi246 <nobusugi246@gmail.com>
2017-05-04 10:19:43 +0900

    using ace editor with uikit.

--- container/src/assets/js/application.js
+++ container/src/assets/js/application.js
@@ -1,9 +1,9 @@
 //This is a JavaScript file with its top level require directives
 //= require jquery-3.2.1
+//= require lodash
 //= require uikit
 //= require uikit-icons
 //= require vue
-//= require ace
 //= require main
 //= require_self
 
--- container/src/main/groovy/gki/container/domain/Bot.groovy
+++ container/src/main/groovy/gki/container/domain/Bot.groovy
@@ -1,23 +1,39 @@
 package gki.container.domain
 
 import groovy.transform.CompileStatic
+import groovy.transform.builder.Builder
 
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.validation.constraints.NotNull
 import java.sql.Timestamp
 
 @CompileStatic
+@Builder
 @Entity
 class Bot {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    private long id
+    long id
 
-    private String name
-    private Timestamp createdDate
-    private Timestamp updatedDate
-    private boolean enabled
-    private byte[] script
+    long revision = 0
+
+    @NotNull
+    String name
+
+    @NotNull
+    String createdBy
+
+    String revisedBy
+
+    @NotNull
+    Timestamp createdDate
+
+    Timestamp updatedDate
+    boolean enabled
+
+    @NotNull
+    String script
 }
